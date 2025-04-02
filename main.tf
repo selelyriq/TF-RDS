dd5e96928d53949fe58d7e1d3bf7999fc38fbfbe
lyriq-cloudsolutions <lyriq@geniouscloudsolutions.com>
2025-04-01 23:43:01 -0400

    rds setup

---
+++ main.tf
@@ -0,0 +1,63 @@
+###########################################################
+# Create RDS Instance
+###########################################################
+
+resource "aws_db_instance" "RDS" {
+  identifier = var.identifier
+  engine = var.engine
+  instance_class = var.instance_class
+  allocated_storage = var.allocated_storage
+  username = var.username
+  password = local.secret_string.password
+  tags = var.tags
+}
+
+###########################################################
+# Create a random password for the RDS instance.
+###########################################################
+
+resource "random_password" "password" {
+  length           = 16
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+
+###########################################################
+# Create RDS Secret
+###########################################################
+
+resource "aws_secretsmanager_secret" "RDS_Secret" {
+  name = "RDS_Secret"
+  description = "Secret for RDS"
+  tags = var.tags
+}
+
+###########################################################
+# Create RDS Secret Version, this sets the password for the RDS instance. Using a random password.
+###########################################################
+
+resource "aws_secretsmanager_secret_version" "RDS_Secret_Version" {
+  secret_id = aws_secretsmanager_secret.RDS_Secret.id
+  secret_string = jsonencode({
+    username = var.username
+    password = random_password.password.result
+  })
+}
+
+###########################################################
+# Get the RDS Secret Version, this is used to get the password for the RDS instance.
+###########################################################
+
+data "aws_secretsmanager_secret_version" "RDS_Secret_Version" {
+  secret_id = aws_secretsmanager_secret.RDS_Secret.id
+
+  depends_on = [aws_secretsmanager_secret_version.RDS_Secret_Version]
+}
+
+###########################################################
+# Get the RDS Secret Version, and decode the secret string.
+###########################################################
+
+locals {
+    secret_string = jsondecode(data.aws_secretsmanager_secret_version.RDS_Secret_Version.secret_string)
+}
---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "tags" {
+    type = map
+}
+
+variable "identifier" {
+    type = string
+}
+
+variable "engine" {
+    type = string
+}
+
+variable "instance_class" {
+    type = string
+}
+
+variable "allocated_storage" {
+    type = number
+}
+
+variable "username" {
+    type = string
+}
